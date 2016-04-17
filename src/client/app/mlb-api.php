5f721adc9ce63d256b3cb8b2983714692af96cc2
Eric Stout <ericwstout@gmail.com>
2016-04-16 19:27:01 -0700

    Updates pinetar localfile for server

--- src/client/app/mlb-api.php
+++ src/client/app/mlb-api.php
@@ -7,7 +7,7 @@ $currentYear = date('Y');
 $currentMonth = date('m');
 $currentDay = date('d');
 
-$localFile = "mlb-scoreboard.json";
+$localFile = "/home/pinetar/src/client/app/mlb-scoreboard.json";
 $fileWrite = fopen($localFile, 'w') or die("Error: can't open file.");
 $stringData = file_get_contents("http://mlb.mlb.com/gdcross/components/game/mlb/year_".$currentYear."/month_".$currentMonth."/day_".$currentDay."/master_scoreboard.json");
 fwrite($fileWrite, $stringData);
