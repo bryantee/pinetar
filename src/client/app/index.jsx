2af843cd1d06f6a75d9a057f27d8f4aa78b5635a
Eric Stout <ericwstout@gmail.com>
2016-04-16 19:44:59 -0700

    Updates index js with new code

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,47 +1,45 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-var Scores = React.createClass({
+var MLBScores = React.createClass({
   getInitialState: function() {
     return {
-         data : undefined
+      hometeam: '',
+      homescore: '',
+      awayteam: '',
+      awayscore: ''
     };
   },
+
   componentDidMount: function() {
-    var scores = this;
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      success: function(data) {
-        if(scores.isMounted()) {
-          scores.setState({
-            data: data
-          });
-        }
-      }
-    });
+
+    this.serverRequest = $.get(this.props.feed, function(result) {
+
+    var scoreFeed = JSON.parse(result).data;
+
+    if( scoreFeed.games.game[0].linescore ){
+    	var homeScore = scoreFeed.games.game[0].linescore.r.home;
+      var awayScore = scoreFeed.games.game[0].linescore.r.away;
+    }
+
+      this.setState({
+        hometeam: scoreFeed.games.game[0].home_team_name,
+        homescore: homeScore,
+        awayteam: scoreFeed.games.game[0].away_team_name,
+        awayscore: awayScore
+      });
+
+    }.bind(this));
   },
-  render : function(){
-  if(this.state.data.data.games.game[0]){
-    var homeTeam = this.state.data.data.games.game[0].home_team_name;
 
-    return(
-      <div>
-        <p>{homeTeam}</p>
-      </div>
-    );
-  } else{
-    return (
-      <div>
-        <p>Hold tight...loading the latest scores.</p>
-      </div>
-    );
-  }
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  },
 
+  render: function() {
+    return ( < div >
+    {this.state.hometeam}{this.state.homescore} vs. { this.state.awayteam}{this.state.awayscore} < /div>
+    );
   }
 });
 
-ReactDOM.render(
-  <Scores url="app/mlb-scoreboard.json"/>,
-  document.getElementById('app')
+ReactDOM.render( < MLBScores feed= "http://198.199.92.64/src/client/app/mlb-scoreboard.json" / > ,
+  document.getElementById('container')
 );
