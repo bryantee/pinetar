68786eaf75b8273ea4053f81fff6721b066a3ec2
Eric Stout <ericwstout@gmail.com>
2016-05-04 21:43:05 -0700

    Adds new app structure

---
+++ assets/js/pinetar.js
@@ -0,0 +1,11 @@
+/* Pinetar by Eric Stout */
+
+class GameBox extends React.Component{
+  render() {
+    return( <div>Hello World</div> );
+  }
+}
+
+ReactDOM.render(
+  <GameBox />, document.getElementById('pinetar')
+);
