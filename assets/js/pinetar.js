af2ea0fddb7da8c47030c442a6771cfa22c7c827
Eric Stout <ericwstout@gmail.com>
2016-05-04 22:10:23 -0700

    Adds get game score functions

--- assets/js/pinetar.js
+++ assets/js/pinetar.js
@@ -1,6 +1,41 @@
 /* Pinetar by Eric Stout */
 
+// class SingleGame extends React.Component{
+//
+// }
+
 class GameBox extends React.Component{
+
+  constructor() {
+    super();
+
+    this.state = {
+      games: []
+    };
+  }
+
+  componentWillMount() {
+    this._getGameScores();
+  }
+
+  componentDidMount() {
+    this._timer = setInterval(() => this._getGameScores(), 45000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this._timer);
+  }
+
+  _getGameScores(){
+    jQuery.ajax({
+      method: 'GET',
+      url: 'http://pinetar-app.com/src/client/app/mlb-scoreboard.json',
+      success: (games) => {
+        this.setState({games})
+      }
+    });
+  }
+
   render() {
     return( <div>Hello World</div> );
   }
