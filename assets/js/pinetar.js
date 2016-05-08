33cf8f7510e28c7f3e09d37227cc1a7877bd6321
Eric Stout <ericwstout@gmail.com>
2016-05-08 16:24:14 -0700

    Adjusts current game card

--- assets/js/pinetar.js
+++ assets/js/pinetar.js
@@ -59,16 +59,51 @@ class CurrentGame extends React.Component{
   render(){
     return(
       <div className="col-6 text-center game-card">
-        <div className="game-card--container">
-        <h2>
-        {this.props.homeTeam} vs. {this.props.awayTeam}
-        </h2>
-        <h4>
-          {this.props.status} | {this.props.inningState} {this.props.inning}
-        </h4>
-        <p>Current Batter: {this.props.currentBatter} <br />
-          Current Pitcher: {this.props.currentPitcher}
-        </p>
+        <div className="game-card--container container">
+
+          <div className="row">
+            <div className="sm-col-8 col-8 text-left">
+              <h2>
+                {this.props.awayTeam}
+              </h2>
+            </div>
+            <div className="sm-col-4 col-4 text-center">
+              <h2>
+                {this.props.awayScore}
+              </h2>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="sm-col-8 col-8 text-left">
+              <h2>
+                {this.props.homeTeam}
+              </h2>
+            </div>
+            <div className="sm-col-4 col-4 text-center">
+              <h2>
+                {this.props.homeScore}
+              </h2>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-12 text-left">
+              <h4>
+                {this.props.status} {this.props.inningState} {this.props.inning}
+              </h4>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-12 text-left game-notes">
+              <p>
+                <strong>Current Pitcher:</strong> {this.props.currentPitcher} <br />
+                <strong>Current Batter: </strong> {this.props.currentBatter}
+              </p>
+            </div>
+          </div>
+
         </div>
       </div>
     );
