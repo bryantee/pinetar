0f2ce1df1c8b517524dd60c3ea8affc39034d425
Eric Stout <ericwstout@gmail.com>
2016-05-08 16:18:27 -0700

    Restructures final game cards

--- assets/js/pinetar.js
+++ assets/js/pinetar.js
@@ -5,46 +5,50 @@ class FinalGame extends React.Component{
     return(
       <div className="col-6 text-center game-card">
         <div className="game-card--container container">
+
           <div className="row">
-            <div className="sm-col-5 col-5 text-center">
+            <div className="sm-col-8 col-8 text-left">
               <h2>
                 {this.props.awayTeam}
               </h2>
             </div>
-            <div className="sm-col-2 col-2 text-center">
-              <h2> vs. </h2>
-            </div>
-            <div className="sm-col-5 col-5 text-center">
-              <h2>
-                {this.props.homeTeam}
-              </h2>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-12 text-center">
-              <h4>
-                {this.props.status} {this.props.inningState} {this.props.inning}
-              </h4>
-            </div>
-          </div>
-          <div className="row">
-            <div className="sm-col-5 col-5 text-center">
+            <div className="sm-col-4 col-4 text-center">
               <h2>
                 {this.props.awayScore}
               </h2>
             </div>
-            <div className="sm-col-5 col-5 text-center">
+          </div>
+
+          <div className="row">
+            <div className="sm-col-8 col-8 text-left">
+              <h2>
+                {this.props.homeTeam}
+              </h2>
+            </div>
+            <div className="sm-col-4 col-4 text-center">
               <h2>
                 {this.props.homeScore}
               </h2>
             </div>
           </div>
 
+          <div className="row">
+            <div className="col-12 text-left">
+              <h4>
+                {this.props.status} {this.props.inningState} {this.props.inning}
+              </h4>
+            </div>
+          </div>
 
-        <p>
-          Winning Pitcher: {this.props.winningPitcher} <br />
-          Losing Pitcher: {this.props.losingPitcher}
-        </p>
+          <div className="row">
+            <div className="col-12 text-left game-notes">
+              <p>
+                <strong>Winning Pitcher:</strong> {this.props.winningPitcher} <br />
+                <strong>Losing Pitcher: </strong> {this.props.losingPitcher}
+              </p>
+            </div>
+          </div>
+
         </div>
       </div>
     );
