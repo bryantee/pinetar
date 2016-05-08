fa973b2a34f804922bd42fc16e77e1cbb8f690ca
Eric Stout <ericwstout@gmail.com>
2016-05-08 14:30:28 -0700

    Adds logo, gulpfile, html changes

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+// Pinetar Gulpfile
+
+var gulp = require('gulp');
+
+var sass          = require('gulp-sass'),
+    nano          = require('gulp-cssnano'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    notify        = require('gulp-notify'),
+    rename        = require('gulp-rename');
+
+// File Paths
+var sassFiles   = ['./assets/scss/main.scss'],
+    cssFiles    = ['./assets/css/*.css', '!./assets/css/*.min.css'];
+
+// Compile Sass
+gulp.task('sass', function() {
+  return gulp.src( sassFiles )
+    .pipe(sourcemaps.init())
+      .pipe(sass()
+        .on('error', sass.logError))
+        .on('error', notify.onError("Error compiling SASS!")
+      )
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      }))
+    .pipe(gulp.dest( './assets/css' ))
+});
+
+// Minimize CSS
+gulp.task('minify-css', ['sass'], function() {
+	return gulp.src( cssFiles )
+  	.pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(nano({
+      discardComments: {removeAll: true},
+      autoprefixer: false
+    }))
+    .pipe(gulp.dest( './assets/css' ))
+});
+
+gulp.task('styles', ['minify-css']);
+
+gulp.task('default', ['styles']);
