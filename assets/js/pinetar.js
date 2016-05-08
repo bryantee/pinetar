8812d67f10b268c25a8153512f310fb31e503e01
Eric Stout <ericwstout@gmail.com>
2016-05-08 14:01:39 -0700

    Adds starting pitchers

--- assets/js/pinetar.js
+++ assets/js/pinetar.js
@@ -43,7 +43,10 @@ class UpcomingGame extends React.Component{
         <h4>
           {this.props.status} | {this.props.inningState} {this.props.inning}
         </h4>
-
+        <p>
+          Starting Pitcher: {this.props.homeStartingPitcher} <br />
+          Starting Pitcher: {this.props.awayStartingPitcher}
+        </p>
       </div>
     );
   }
@@ -98,8 +101,8 @@ class GameBox extends React.Component{
         let status = games.home_time + ' ' + games.home_time_zone, // Change the Preview status text to show Start Time
             inningState = '',
             inning = '';
-            //homeStartingPitcher = games.home_probable_pitcher.name_display_roster,
-            //awayStartingPitcher = games.away_probable_pitcher.name_display_roster;
+            homeStartingPitcher = games.home_probable_pitcher.name_display_roster,
+            awayStartingPitcher = games.away_probable_pitcher.name_display_roster;
 
         return(
           <UpcomingGame
@@ -113,6 +116,8 @@ class GameBox extends React.Component{
 
       } else if ( status == 'Final' || status == 'Game Over') {
         // if the game is final
+        status = 'Final';
+
         return(
           <FinalGame
             homeTeam={homeTeam}
